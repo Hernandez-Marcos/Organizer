bf15fdb106e4054c0fdcab98798f0fda7a2a34a7
SgtFreddy713 <captesxel@gmail.com>
2025-12-19 01:08:01 -0300

    Now you can see your tasks in "tasks".

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
 #@login_required
 def index(request):
-    return render(request, "tasks/index.html")
+    tasks = Task.objects.filter(user=request.user).order_by("-id")
+    context = {"tasks": tasks}
+    return render(request, "tasks/index.html", context)
 
 
 @login_required
@@ -20,6 +23,6 @@ def createTask(request):
             return redirect('tasks:index')
     else:
         form = TaskForm()
-        
+
     return render(request, 'tasks/form.html', {"form": form})
  
